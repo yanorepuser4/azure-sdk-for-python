2636b828721d6fc9fe0e4d1bad451db6da220b66
Matt Metcalf <mrm9084@gmail.com>
2024-03-27 13:01:33 -0700

    formating & non live tests

--- sdk/appconfiguration/azure-appconfiguration-provider/tests/test_dns_python.py
+++ sdk/appconfiguration/azure-appconfiguration-provider/tests/test_dns_python.py
@@ -5,24 +5,19 @@ import dns.resolver
 
 
 class TestDnsPython(AppConfigTestCase):
-
     @recorded_by_proxy
     @app_config_decorator_aad
     def test(self, appconfiguration_endpoint_string):
         uri = appconfiguration_endpoint_string[8:]
 
-        answers = dns.resolver.resolve(
-            "_origin._tcp." + uri, "SRV"
-        )
+        answers = dns.resolver.resolve("_origin._tcp." + uri, "SRV")
         main_count = 0
         for server in answers:
             print(server.target, server.port, server.priority, server.weight)
             main_count += 1
         assert main_count == 1
 
-        answers = dns.resolver.resolve(
-            "_alt0._tcp." + uri, "SRV"
-        )
+        answers = dns.resolver.resolve("_alt0._tcp." + uri, "SRV")
         replica_count = 0
         for server in answers:
             print(server.target, server.port, server.priority, server.weight)
